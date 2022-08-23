7336e3ed1181625ffaeccdba85ce48d2a35f3d44
harishm7 <hmateti@digisprint.com>
2022-08-23 18:37:21 +0530

    middleware

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 var express =require('express');
 var app=express();
+
+// types of middleware
+// -application level middleware
+// -Router level middleware
+// -Error handling middleware
+// -Built in middleware
+// -Third party middleware
+
+const middlewarefun=(req,res,next)=>{
+ if(!req.query.age){
+    res.send("please provide age")
+ }
+ else if(req.query.age<18){
+    res.send("you can not access this site")
+ }
+ else{
+    next()
+ }
+}
+
+// app.use(middlewarefun)   // application level middleware
+
 app.get('/',(req,res)=>{
     res.send("home page")
 })
 
-app.get('/about',(req,res)=>{
+app.get('/user',middlewarefun,(req,res)=>{
     res.send("<h1>about page </h1>")
 })
+
 
 app.listen(8080);
